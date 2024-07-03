0d175f9c18193e75e6fbe1e06f0eb69abf7f7c67
asad <asad009xa@gmail.com>
2024-07-04 02:29:04 +0300

    dfs

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+// const slides = document.querySelectorAll('.slide');
+// let currentSlide = 0;
+
+// function showSlide(slideIndex) {
+//     slides.forEach((slide) => slide.style.display = 'none');
+//     slides[slideIndex].style.display = 'block';
+// }
+
+// function nextSlide() {
+//     currentSlide++;
+//     if (currentSlide >= slides.length) {
+//         currentSlide = 0;
+//     }
+//     showSlide(currentSlide);
+// }
+
+// function prevSlide() {
+//     currentSlide--;
+//     if (currentSlide < 0) {
+//         currentSlide = slides.length - 1;
+//     }
+//     showSlide(currentSlide);
+// }
+
+// document.querySelector('.next').addEventListener('click', nextSlide);
+// document.querySelector('.prev').addEventListener('click', prevSlide);
+
+// showSlide(currentSlide);
